1d59a51fbe0e135fd275e0f31fb17568b50d812a
victor Farias <victorfarias.tv@gmail.com>
2021-09-23 11:28:16 -0300

    Updates

---
+++ precache-manifest.425ef46c28bf8a8cdc0513d4a3773212.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e807713a87c7d13624a04bd1fd3a45d7",
+    "url": "/starwars-planets-search/index.html"
+  },
+  {
+    "revision": "74a9c430c3239ccccc18",
+    "url": "/starwars-planets-search/static/css/main.e45818df.chunk.css"
+  },
+  {
+    "revision": "880af3b0277bacb75d9e",
+    "url": "/starwars-planets-search/static/js/2.df83ad31.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets-search/static/js/2.df83ad31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a9c430c3239ccccc18",
+    "url": "/starwars-planets-search/static/js/main.aa5182ff.chunk.js"
+  },
+  {
+    "revision": "1b9e9e3eb9707d3157b0",
+    "url": "/starwars-planets-search/static/js/runtime-main.d5e8347d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c78641996920cbb7dd74ef0e67a947e0.js"
+  "/starwars-planets-search/precache-manifest.425ef46c28bf8a8cdc0513d4a3773212.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/starwars-planets-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
